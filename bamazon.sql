ac77426c4f3727696f5d35335333557fa467740e
Technophile <patricia.snodgrass@gmail.com>
2018-05-21 22:45:37 -0500

    adding updated app files

--- bamazon.sql
+++ bamazon.sql
@@ -8,16 +8,17 @@ CREATE TABLE products (
   item_id INT NOT NULL AUTO_INCREMENT,
   product_name VARCHAR(50) NULL,
   department_name VARCHAR(50) NULL,
-  price DECIMAL(10,2) NULL,
+  price DECIMAL(10,2) DEFAULT 0.00,
   stock_quantity INT NULL,
-  product_sales INT default 0,
+  product_sales decimal(10,2) DEFAULT 0.00,
   PRIMARY KEY (item_id)
 );
 
 CREATE TABLE departments (
     department_id INT NOT NULL AUTO_INCREMENT,
     department_name VARCHAR(50) NULL,
-    over_head_costs INT default 0,
+    over_head_costs decimal(10,2) DEFAULT 0.00,
+    PRIMARY KEY (department_id)
 )
 
 INSERT INTO products (product_name, department_name, price, quantity)
